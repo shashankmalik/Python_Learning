fa1d594f2e423fbb67055ba10cd2c7291b8d2746
shashankmalik <87019325+shashankmalik@users.noreply.github.com>
2021-08-20 03:02:38 -0400

    Update Module2_Assessment_10.py

--- Week_2/Module2_Assessment_10.py
+++ Week_2/Module2_Assessment_10.py
@@ -1,8 +1,8 @@
 def fractional_part(numerator, denominator):
+	if denominator > 0 :
+		return (numerator/denominator - numerator//denominator)
 	# Operate with numerator and denominator to 
 # keep just the fractional part of the quotient
-	if denominator > 0 :
-		return (numerator%denominator)/denominator
 	return 0
 
 print(fractional_part(5, 5)) # Should be 0
