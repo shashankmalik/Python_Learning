b0588dd9645e7958fe196796515455d414cabfe5
shashankmalik <87019325+shashankmalik@users.noreply.github.com>
2021-08-20 00:37:03 -0400

    Create The Principles of Code Reuse.py
    In this code, identify the repeated pattern and replace it with a function called month_days, that receives the name of the month and the number of days in that month as parameters. Adapt the rest of the code so that the result is the same. Confirm your results by making a function call with the correct parameters for both months listed.

---
+++ Week_2/The Principles of Code Reuse.py
@@ -0,0 +1,11 @@
+# REPLACE THIS STARTER CODE WITH YOUR FUNCTION
+june_days = 30
+print("June has " + str(june_days) + " days.")
+july_days = 31
+print("July has " + str(july_days) + " days.")
+
+
+def month_days(month,days):
+    print(month +" has "+ str(days) +" days.")
+month_days("June",30)
+month_days("July",31)
