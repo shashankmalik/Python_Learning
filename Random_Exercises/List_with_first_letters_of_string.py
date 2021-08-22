198c22017984e2442aba126ff8dbff2500eca3bb
shashankmalik <87019325+shashankmalik@users.noreply.github.com>
2021-08-22 06:42:51 -0400

    Create List_with_first_letters_of_string.py

---
+++ Random_Exercises/List_with_first_letters_of_string.py
@@ -0,0 +1,6 @@
+#Create a list of the first letters of every word in this string
+st = 'Create a list of the first letters of every word in this string'
+#Code in this cell
+x=st.split()
+result = [y[0] for y in x]
+result
