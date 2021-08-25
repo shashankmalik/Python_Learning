bc1e486316290d1acc277dec1defd2472e9fc9dc
shashankmalik <87019325+shashankmalik@users.noreply.github.com>
2021-08-25 04:36:45 -0400

    Create Practice Quiz: Lists_6.py
    The guest_list function reads in a list of tuples with the name, age, and profession of each party guest, and prints the sentence "Guest is X years old and works as __." for each one. For example, guest_list(('Ken', 30, "Chef"), ("Pat", 35, 'Lawyer'), ('Amanda', 25, "Engineer")) should print out: Ken is 30 years old and works as Chef. Pat is 35 years old and works as Lawyer. Amanda is 25 years old and works as Engineer. Fill in the gaps in this function to do that.

---
+++ Week_4/Practice Quiz: Lists_6.py
@@ -0,0 +1,21 @@
+# The guest_list function reads in a list of tuples with the name, age, and profession of each party guest, and prints the sentence "Guest is X years old and works as __." for each one. For example, guest_list(('Ken', 30, "Chef"), ("Pat", 35, 'Lawyer'), ('Amanda', 25, "Engineer")) should print out: Ken is 30 years old and works as Chef. Pat is 35 years old and works as Lawyer. Amanda is 25 years old and works as Engineer. Fill in the gaps in this function to do that. 
+
+
+
+def guest_list(guests):
+	x= []
+	for x in guests:
+		a = x[0]
+		b = x[1]
+		c = x[2]
+		
+		print("{} is {} years old and words as {}".format(a,b,c))
+
+guest_list([('Ken', 30, "Chef"), ("Pat", 35, 'Lawyer'), ('Amanda', 25, "Engineer")])
+
+"""
+Output should match:
+Ken is 30 years old and works as Chef
+Pat is 35 years old and works as Lawyer
+Amanda is 25 years old and works as Engineer
+"""
